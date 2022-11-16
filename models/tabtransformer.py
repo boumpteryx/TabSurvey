f954d28d02233fc8b98ecf5e8f3d1ab0fe5519ed
Salah GHAMIZI <salah.ghamizi@gmail.com>
2022-11-16 16:47:14 +0100

    Support for balanced loss training

--- models/tabtransformer.py
+++ models/tabtransformer.py
@@ -12,6 +12,35 @@ import numpy as np
     Code adapted from: https://github.com/lucidrains/tab-transformer-pytorch
 '''
 
+class BalancedBCELoss(torch.nn.BCEWithLogitsLoss):
+
+    def __init__(self, dataset, **args):
+        self.weights = None
+        if dataset in ["url", "malware", "ctu_13_neris", "lcld_v2_time"]:
+            from constrained_attacks import datasets
+            _, y = datasets.load_dataset(dataset).get_x_y()
+            y = np.array(y)
+            y_class, y_occ = np.unique(y, return_counts=True)
+            self.weights = dict(zip(y_class, y_occ/ len(y)))
+            print(self.weights)
+        super(BalancedBCELoss, self).__init__(**args)
+
+    def forward(self, input: torch.Tensor, target: torch.Tensor) -> torch.Tensor:
+
+        if self.weights is None:
+            return super(BalancedBCELoss, self).forward(input, target)
+
+        negative_inputs_mask= (target==0)
+        positive_inputs_mask = (target == 1)
+
+        positive_inputs, positive_targets = input[positive_inputs_mask], input[positive_inputs_mask]
+        positive_loss = super(BalancedBCELoss, self).forward(positive_inputs,positive_targets)
+        negative_inputs, negative_targets = input[negative_inputs_mask], input[negative_inputs_mask]
+        negative_loss = super(BalancedBCELoss, self).forward(negative_inputs, negative_targets)
+        
+        return positive_loss/self.weights.get(1) + negative_loss / self.weights.get(0)
+        
+
 
 class TabTransformer(BaseModelTorch):
 
@@ -34,6 +63,7 @@ class TabTransformer(BaseModelTorch):
         self.batch_size = self.args.batch_size if args.num_features < 50 else 64
 
         print("Using dim %d and batch size %d" % (dim, self.batch_size))
+        self.dataset =args.dataset
 
         self.model = TabTransformerModel(
             categories=categories_unique,  # tuple (or list?) containing the number of unique values in each category
@@ -72,7 +102,7 @@ class TabTransformer(BaseModelTorch):
         elif self.args.objective == "classification":
             loss_func = nn.CrossEntropyLoss()
         else:
-            loss_func = nn.BCEWithLogitsLoss()
+            loss_func = BalancedBCELoss(self.dataset)
             y = y.float()
             y_val = y_val.float()
 
