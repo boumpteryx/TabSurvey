c87e6cd98e15cebe78e6e8e36359f5dcb111fb43
Salah GHAMIZI <salah.ghamizi@gmail.com>
2022-11-23 20:59:55 +0100

    Added Tensorflow version of the weighted loss

--- models/tabtransformer.py
+++ models/tabtransformer.py
@@ -12,7 +12,51 @@ import numpy as np
     Code adapted from: https://github.com/lucidrains/tab-transformer-pytorch
 '''
 
-class BalancedBCELoss(torch.nn.BCEWithLogitsLoss):
+### Keras / Tensorflow loss
+import keras
+import tensorflow as tf
+class BalancedBCELossKeras(keras.losses.Loss):
+    """
+    Args:
+      pos_weight: Scalar to affect the positive labels of the loss function.
+      weight: Scalar to affect the entirety of the loss function.
+      from_logits: Whether to compute loss from logits or the probability.
+      reduction: Type of tf.keras.losses.Reduction to apply to loss.
+      name: Name of the loss function.
+    """
+    def __init__(self, dataset, from_logits=False,
+                 reduction=keras.losses.Reduction.AUTO,
+                 name='weighted_binary_crossentropy'):
+        super().__init__(reduction=reduction, name=name)
+        if dataset in ["url", "malware", "ctu_13_neris", "lcld_v2_time"]:
+            from constrained_attacks import datasets
+            _, y = datasets.load_dataset(dataset).get_x_y()
+            y = np.array(y)
+            y_class, y_occ = np.unique(y, return_counts=True)
+            self.weights = dict(zip(y_class, y_occ / len(y)))
+        self.from_logits = from_logits
+
+    def call(self, target, input):
+
+        if self.weights is None:
+            ce = tf.losses.binary_crossentropy(input, target, from_logits=self.from_logits)[:,None]
+
+        else:
+            negative_inputs_mask = (target == 0)
+            positive_inputs_mask = (target == 1)
+
+            positive_inputs, positive_targets = input[positive_inputs_mask], input[positive_inputs_mask]
+            positive_loss = tf.losses.binary_crossentropy(positive_inputs, positive_targets, from_logits=self.from_logits)[:,None]
+            negative_inputs, negative_targets = input[negative_inputs_mask], input[negative_inputs_mask]
+            negative_loss = tf.losses.binary_crossentropy(negative_inputs, negative_targets, from_logits=self.from_logits)[:,None]
+
+            ce = positive_loss / self.weights.get(1) + negative_loss / self.weights.get(0)
+
+        return ce
+
+
+### Pytorch loss
+class BalancedBCELossPytorch(torch.nn.BCEWithLogitsLoss):
 
     def __init__(self, dataset, **args):
         self.weights = None
@@ -22,21 +66,21 @@ class BalancedBCELoss(torch.nn.BCEWithLogitsLoss):
             y = np.array(y)
             y_class, y_occ = np.unique(y, return_counts=True)
             self.weights = dict(zip(y_class, y_occ/ len(y)))
-            print(self.weights)
-        super(BalancedBCELoss, self).__init__(**args)
+
+        super(BalancedBCELossPytorch, self).__init__(**args)
 
     def forward(self, input: torch.Tensor, target: torch.Tensor) -> torch.Tensor:
 
         if self.weights is None:
-            return super(BalancedBCELoss, self).forward(input, target)
+            return super(BalancedBCELossPytorch, self).forward(input, target)
 
         negative_inputs_mask= (target==0)
         positive_inputs_mask = (target == 1)
 
         positive_inputs, positive_targets = input[positive_inputs_mask], input[positive_inputs_mask]
-        positive_loss = super(BalancedBCELoss, self).forward(positive_inputs,positive_targets)
+        positive_loss = super(BalancedBCELossPytorch, self).forward(positive_inputs,positive_targets)
         negative_inputs, negative_targets = input[negative_inputs_mask], input[negative_inputs_mask]
-        negative_loss = super(BalancedBCELoss, self).forward(negative_inputs, negative_targets)
+        negative_loss = super(BalancedBCELossPytorch, self).forward(negative_inputs, negative_targets)
         
         return positive_loss/self.weights.get(1) + negative_loss / self.weights.get(0)
         
