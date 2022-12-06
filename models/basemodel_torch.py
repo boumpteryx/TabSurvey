e449b746fc99848124ae9e73c658a2051bcecadb
Antoine Desjardins <44445863+boumpteryx@users.noreply.github.com>
2022-12-06 11:14:00 +0100

    updates from Salah

--- models/basemodel_torch.py
+++ models/basemodel_torch.py
@@ -116,14 +116,14 @@ class BaseModelTorch(BaseModel):
         self.load_model(filename_extension="best", directory="tmp")
         return loss_history, val_loss_history
 
-    def predict(self, X):
+    def predict(self, X, return_torch=False):
         if self.args.objective == "regression":
             self.predictions = self.predict_helper(X)
         else:
             self.predict_proba(X)
             self.predictions = np.argmax(self.prediction_probabilities, axis=1)
 
-        return self.predictions
+        return torch.Tensor(self.predictions) if return_torch else self.predictions
 
     def predict_proba(self, X: np.ndarray) -> np.ndarray:
         probas = self.predict_helper(X)
@@ -135,7 +135,11 @@ class BaseModelTorch(BaseModel):
         self.prediction_probabilities = probas
         return self.prediction_probabilities
 
-    def predict_helper(self, X):
+    def predict_torch(self, X):
+
+        return self.predict_helper(X.to(self.device), True)
+
+    def predict_helper(self, X, keep_grad=False):
         self.model.eval()
 
         X = torch.tensor(X).float()
--- models/deepfm.py
+++ models/deepfm.py
@@ -5,7 +5,6 @@ import numpy as np
 from .basemodel_torch import BaseModelTorch
 from .deepfm_lib.models.deepfm import DeepFM as DeepFMModel
 from .deepfm_lib.inputs import SparseFeat, DenseFeat
-from utils.losses import BalancedBCELossPytorch as BalancedBCELoss
 
 '''
     DeepFM: A Factorization-Machine based Neural Network for CTR Prediction
@@ -13,6 +12,36 @@ from utils.losses import BalancedBCELossPytorch as BalancedBCELoss
      
     Code adapted from: https://github.com/shenweichen/DeepCTR-Torch
 '''
+
+class BalancedBCELoss(torch.nn.BCEWithLogitsLoss):
+
+    def __init__(self, dataset, **args):
+        self.weights = None
+        if dataset in ["url", "malware", "ctu_13_neris", "lcld_v2_time"]:
+            from constrained_attacks import datasets
+            _, y = datasets.load_dataset(dataset).get_x_y()
+            y = np.array(y)
+            y_class, y_occ = np.unique(y, return_counts=True)
+            self.weights = dict(zip(y_class, y_occ / len(y)))
+            print(self.weights)
+        super(BalancedBCELoss, self).__init__(**args)
+
+    def forward(self, input: torch.Tensor, target: torch.Tensor, reduction=None) -> torch.Tensor:
+
+        if self.weights is None:
+            return super(BalancedBCELoss, self).forward(input, target)
+
+        negative_inputs_mask = (target == 0)
+        positive_inputs_mask = (target == 1)
+
+        positive_inputs, positive_targets = input[positive_inputs_mask], input[positive_inputs_mask]
+        positive_loss = super(BalancedBCELoss, self).forward(positive_inputs, positive_targets)
+        negative_inputs, negative_targets = input[negative_inputs_mask], input[negative_inputs_mask]
+        negative_loss = super(BalancedBCELoss, self).forward(negative_inputs, negative_targets)
+
+        return positive_loss / self.weights.get(1) + negative_loss / self.weights.get(0)
+
+
 
 class DeepFM(BaseModelTorch):
 
@@ -72,14 +101,22 @@ class DeepFM(BaseModelTorch):
                                                         patience=self.args.early_stopping_rounds)
         return loss_history, val_loss_history
 
-    def predict_helper(self, X):
-        X = np.array(X, dtype=np.float)
-        X_dict = {str(name): X[:, name] for name in range(self.args.num_features)}
+    def predict_helper(self, X, keep_grad=False):
+        if keep_grad:
+            if not self.args.cat_idx:
+                X_formatted = torch.cat((torch.zeros(X.shape[0],1),X),1)
+            else:
+                X_formatted = X
+            out = self.model(X_formatted)
+            return out
+        else:
+            X = np.array(X, dtype=np.float)
+            X_dict = {str(name): X[:, name] for name in range(self.args.num_features)}
 
-        # Adding dummy spare feature
-        if not self.args.cat_idx:
-            X_dict["dummy"] = np.zeros(X.shape[0])
-        return self.model.predict(X_dict, batch_size=self.args.batch_size)
+            # Adding dummy spare feature
+            if not self.args.cat_idx:
+                X_dict["dummy"] = np.zeros(X.shape[0])
+            return self.model.predict(X_dict, batch_size=self.args.batch_size)
 
     @classmethod
     def define_trial_parameters(cls, trial, args):
